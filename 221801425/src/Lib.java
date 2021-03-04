18c22bc8aa4977d5885493863d3daefdacefe8d1
zwh1426 <1515071283@qq.com>
2021-03-04 17:06:35 +0800

    第十次commit

--- 221801425/src/Lib.java
+++ 221801425/src/Lib.java
@@ -6,14 +6,15 @@ import java.util.regex.Pattern;
 
 public class Lib{
     Lib lib=new Lib();
+
     //单词正则表达式
-    private static String WORDS_RE = "[a-zA-Z]{4,}[a-zA-Z0-9]*";
+    private static String word = "[a-zA-Z]{4,}[a-zA-Z0-9]*";
     //分隔符正则表达式
-    private static String BREAK_RE = "[^a-zA-Z0-9]";
+    private static String Break = "[^a-zA-Z0-9]";
     //非空行正则表达式
-    private static String LINE_RE = "(^|\n)\\s*\\S+";
+    private static String line = "(^|\n)\\s*\\S+";
     //获取当前工程的路径
-    public static String DIR = System.getProperty("user.dir");
+    public static String dir = System.getProperty("user.dir");
     //Map表用于存放单词以及相对应的个数
     private static Map<String, Integer> wordsMap = new HashMap<String, Integer>();
 
@@ -34,56 +35,82 @@ public class Lib{
         }catch(IOException e) {
             e.printStackTrace();
         }finally {
-            try {
                 br.close();
-            }catch(IOException e) {
-                e.printStackTrace();
             }
         }
         return sbuilder.toString();
     }
 
-    public static int getCharactersCount(String str) {
+    public static int getCharactersNum(String str) {
         //字符数统计数量
-        int count = 0;
-        char[] ch = str.toCharArray();
-        for(int i = 0; i < ch.length; i++) {
-            if(ch[i] >= 0 && ch[i] <= 127) {
-                count++;
-            }else continue;
-        }
-        return count;
-    }
-
-    public static int getWordsCount(String str) {
-        //单词数的统计量
-        int count = 0;
-        String[] strs = str.split(BREAK_RE);
-        for(int i = 0; i < strs.length; i++) {
-            if(strs[i].matches(WORDS_RE)) {
-                String temp = strs[i].toLowerCase();
-                if(wordsMap.containsKey(temp)) {
-                    int num = wordsMap.get(temp);
-                    wordsMap.put(temp, 1 + num);
-                }
-                else {
-                    wordsMap.put(temp, 1);
-                }
-                count++;
+        int num = 0;
+        char[] temp = str.toCharArray();
+        for(int i = 0; i < temp.length; i++) {
+            if(temp[i] >= 0 && temp[i] <= 127) {
+                num++;
             }
         }
+        return num;
+    }
+
+    public static int getWordsNum(String str) {
+        //单词数的统计量
+        int num = 0;
+        String[] temp1 = str.split(Break);
+        for(int i = 0; i < temp1.length; i++) {
+            if(temp1[i].matches(word))
+            {
+                num++;
+                String temp = strs[i].toLowerCase();
+                if(wordsMap.containsKey(temp))
+                {
+                    int j = wordsMap.get(temp);
+                    wordsMap.put(temp, j+1);
+                }
+                else
+                    {
+                    wordsMap.put(temp, 1);
+                }
+            }
+        }
+        return num;
+    }
+
+    public static int getLineNum(String str) {
+        //行数统计数量
+        File file=new File(str);
+        int count=0;
+
+        if(file.exists())
+        {
+            try
+            {
+                BufferedReader in = new BufferedReader(new FileReader(file));
+                String line;
+                while((line = in.readLine()) != null)
+                {
+                    if(!line.equals("") )
+                    {
+                        count ++;
+                    }
+                }
+                in.close();
+            }
+            catch(FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+
         return count;
     }
 
-    public static int getLineCount(String str) {
-        //行数统计数量
-        int count = 0;
-        Matcher matcher = Pattern.compile(LINE_RE).matcher(str);
-        while(matcher.find()) {
-            count++;
-        }
-        return count;
-    }
+}
 
     public static List<Map.Entry<String, Integer>> sortHashmap() {
         List<Map.Entry<String, Integer>> list;
@@ -99,19 +126,20 @@ public class Lib{
     }
 
     public static void writeToFile(int characters, int words, int lines, String filePath) {
+        FileOutputStream fos = null;
+        OutputStreamWriter writer = null;
+        BufferedWriter bw = null;
         String str = "characters: " + characters + "\nwords: " + words + "\nlines: " + lines +"\n";
         List<Map.Entry<String, Integer>> list = sortHashmap();
         int i = 0;
-        for(Map.Entry<String, Integer> map : list) {
-            if(i < 10) {
+        for(Map.Entry<String, Integer> map : list)
+        {
+            if(i < 10)
+            {
                 str += map.getKey() + ": " + map.getValue() + "\n";
                 i++;
             }else break;
         }
-
-        FileOutputStream fos = null;
-        OutputStreamWriter writer = null;
-        BufferedWriter bw = null;
 
         try {
             fos = new FileOutputStream(filePath);
--- 221801425/src/WordCount.java
+++ 221801425/src/WordCount.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-
+import java.io.*;
 public class WordCount {
     String inputFile = "";
     String outputFile = "";
@@ -15,9 +14,9 @@ public class WordCount {
 
     //变量的初始化
     public void init() {
-        this.characters = Lib.getCharactersCount(Lib.readFromFile(this.inputFile));
-        this.lines = Lib.getLineCount(Lib.readFromFile(this.inputFile));
-        this.words = Lib.getWordsCount(Lib.readFromFile(this.inputFile));
+        this.characters = Lib.getCharactersNum(Lib.readFromFile(this.inputFile));
+        this.lines = Lib.getLineNum(Lib.readFromFile(this.inputFile));
+        this.words = Lib.getWordsNum(Lib.readFromFile(this.inputFile));
     }
 
     public void run() {
@@ -26,15 +25,23 @@ public class WordCount {
 
     public static void main(String[] args) {
         if(args.length < 2) {
-            System.out.println("参数不足,请重新运行！");
+            System.out.println("请重新运行！");
         }
         else {
-            long starttime = System.currentTimeMillis();
+            long long starttime = System.currentTimeMillis();
             WordCount wordCount = new WordCount(args[0], args[1]);
             wordCount.init();
             wordCount.run();
-            long endtime = System.currentTimeMillis();
-            System.out.println("time costs: " + (endtime - starttime) + "ms");
+            long long endtime = System.currentTimeMillis();
+            System.out.println("运行时间为: " + (endtime - starttime) + "ms");
+        }
+        int i = 0;
+        for(Map.Entry<String, Integer> map : list) {
+            assertEquals(map.getKey(),result[i]);
+            i++;
+            System.out.println(map.getKey()+" : "+map.getValue());
         }
     }
 }
+
+
